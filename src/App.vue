dd493a76af532b649683c0c010465d658fabbbd4
maya <you@example.com>
2024-03-09 19:36:19 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PtoductDetails />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PtoductDetails from './components/PtoductDetails.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PtoductDetails
   }
 }
 </script>
